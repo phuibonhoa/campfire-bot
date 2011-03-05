75c77057883edcd37f4012d3b7eece9d1093af9d
Michael Pearce <michael.pearce@bookrenter.com>
2011-03-05 12:52:40 -0800

    Do you know speaks html.

--- plugins/do_you_know.rb
+++ plugins/do_you_know.rb
@@ -2,16 +2,17 @@ require 'uri'
 require 'open-uri'
 require 'active_support'
 require 'zlib'
+require 'nokogiri'
 
 class DoYouKnow < CampfireBot::Plugin
   on_command 'do you know', :do_you_know_command
   
   def do_you_know_command(msg)
     do_you_know(msg[:message]).each do |statement|
-      msg.speak(statement)
+      msg.html(statement)
     end
   end
-  
+
 private
 
   def fetch_json_hash(url, deflate = false)
@@ -21,39 +22,60 @@ private
   end
 
   def dont_know_answer(question)
-    message = config['dont_know_responses'].rand || "Sorry, I don't know the answer to that.  Try"
+    message = (config['dont_know_responses'] || ["Sorry, I don't know the answer to that.  Try"]).shuffle.first
     return "#{message} #{googling_link(question)}."
   end
 
   def googling_link(question)
-    "<a href='http://www.google.com?q=#{URI.escape(question)}'>googling it</a>"
+    "<a href='http://www.google.com/#q=#{URI.escape(question)}'>googling it</a>"
   end
-
+  
   def do_you_know(question)
     statements = []
     begin
-      search_response = fetch_json_hash("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=1&key=AIzaSyBv1_qJoAOpBZg3UQ55ebSApmIOmSoFs3Q&q=site:stackoverflow.com%20#{URI.escape(question)}")
-      search_result = search_response['responseData']['results'][0]
-      if search_result
-        question_id = search_result['url'].sub(/^.*questions\//, '').sub(/\/.*$/, '')
+      search_result_url = fetch_question_url(question)
+      if search_result_url
+        question_id = search_result_url.sub(/^.*questions\//, '').sub(/\/.*$/, '')
         question_response = fetch_json_hash("http://api.stackoverflow.com/1.1/questions/#{question_id}?body=true", true)
-        question = "I know this: #{question_response['questions'][0]['title']}"
-        question = "#{question} <a href='http://stackoverflow.com/questions/#{question_id}'>*</a>"
-      
+        question_i_know = "I know this: #{question_response['questions'][0]['title']}"
+        
         answer_response = fetch_json_hash("http://api.stackoverflow.com/1.1/questions/#{question_id}/answers?sort=votes&body=true&pagesize=1", true)
-        answer = answer_response['answers'][0]
-        if answer
-          statements << question
-          statements << answer['body']
+        has_accepted_answer = answer_response['answers'].detect {|a| a['accepted'] || a['up_vote_count'] > 3}
+        if has_accepted_answer
+          # take the most up voted, not the one chosen by question asker
+          statements << answer_response['answers'][0]['body']
+          statements << "Here's <a href='http://stackoverflow.com/questions/#{question_id}'>where I found the information</a>"
         end
       end
       if statements.empty?
         statements << dont_know_answer(question)
       end
     rescue Exception => e
-      statements << "Sorry, my brain hurts. I think it has to do with #{e}."
+      statements << "Sorry, my brain hurts. I think it has to do with #{e}: #{e.backtrace[0]}."
     end
   
     return statements
   end
+  
+  def fetch_question_url(question)
+    doc = Nokogiri::HTML(open("http://stackoverflow.com/search?q=#{URI.escape(question)}"))
+    path = doc.css('a.question-hyperlink').first['href']
+    if path
+      url = "http://stackoverflow.com" + path.gsub(/^.*?\/questions\//, '/questions/')
+    end
+    url
+  end
 end
+
+# a_question = 'What is the most popular programming language?'
+# a_question = 'How to install a plugin from github?'
+# a_question = 'What is the best ruby editor for a mac?'
+# a_question = 'the top companies in the bay area?'
+# a_question = 'why activerecord has an error'
+# a_question = 'best open source iphone libraries' # unordered list
+# a_question = 'why won\'t foo make assignment' # code blocks
+# msg = CampfireBot::Message.new(:message => a_question)
+# def msg.paste(text); puts "-+--#{text}" end
+# def msg.speak(text); puts "-+--#{text}" end
+# DoYouKnow.new.do_you_know_command(msg)
+# raise "EXIT"
